96da85f20b5d41b4fab1ba632158e8015fd70187
NiceFries <100709039+NiceFries@users.noreply.github.com>
2022-08-30 10:25:24 +0600

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,153 @@
+function on1() {
+    document.getElementById("overlay1").style.display = "flex";
+}
+function off1() {
+    document.getElementById("overlay1").style.display = "none";
+}
+
+function on2() {
+    document.getElementById("overlay2").style.display = "flex";
+}
+function off2() {
+    document.getElementById("overlay2").style.display = "none";
+}
+
+function on3() {
+    document.getElementById("overlay3").style.display = "flex";
+}
+function off3() {
+    document.getElementById("overlay3").style.display = "none";
+}
+
+function on4() {
+    document.getElementById("overlay4").style.display = "flex";
+}
+function off4() {
+    document.getElementById("overlay4").style.display = "none";
+}
+
+function on5() {
+    document.getElementById("overlay5").style.display = "flex";
+}
+function off5() {
+    document.getElementById("overlay5").style.display = "none";
+}
+
+function on6() {
+    document.getElementById("overlay6").style.display = "flex";
+}
+function off6() {
+    document.getElementById("overlay6").style.display = "none";
+}
+
+function on6() {
+    document.getElementById("overlay6").style.display = "flex";
+}
+function off6() {
+    document.getElementById("overlay6").style.display = "none";
+}
+
+function on6() {
+    document.getElementById("overlay6").style.display = "flex";
+}
+function off6() {
+    document.getElementById("overlay6").style.display = "none";
+}
+
+function on7() {
+    document.getElementById("overlay7").style.display = "flex";
+}
+function off7() {
+    document.getElementById("overlay7").style.display = "none";
+}
+
+function on8() {
+    document.getElementById("overlay8").style.display = "flex";
+}
+function off8() {
+    document.getElementById("overlay8").style.display = "none";
+}
+
+function on9() {
+    document.getElementById("overlay9").style.display = "flex";
+}
+function off9() {
+    document.getElementById("overlay9").style.display = "none";
+}
+
+function on10() {
+    document.getElementById("overlay10").style.display = "flex";
+}
+function off10() {
+    document.getElementById("overlay10").style.display = "none";
+}
+
+function on11() {
+    document.getElementById("overlay11").style.display = "flex";
+}
+function off11() {
+    document.getElementById("overlay11").style.display = "none";
+}
+
+function on12() {
+    document.getElementById("overlay12").style.display = "flex";
+}
+function off12() {
+    document.getElementById("overlay12").style.display = "none";
+}
+
+function on13() {
+    document.getElementById("overlay13").style.display = "flex";
+}
+function off13() {
+    document.getElementById("overlay13").style.display = "none";
+}
+
+function on14() {
+    document.getElementById("overlay14").style.display = "flex";
+}
+function off14() {
+    document.getElementById("overlay14").style.display = "none";
+}
+
+function on15() {
+    document.getElementById("overlay15").style.display = "flex";
+}
+function off15() {
+    document.getElementById("overlay15").style.display = "none";
+}
+
+function on16() {
+    document.getElementById("overlay16").style.display = "flex";
+}
+function off16() {
+    document.getElementById("overlay16").style.display = "none";
+}
+
+function on17() {
+    document.getElementById("overlay17").style.display = "flex";
+}
+function off17() {
+    document.getElementById("overlay17").style.display = "none";
+}
+
+function on18() {
+    document.getElementById("overlay18").style.display = "flex";
+}
+function off18() {
+    document.getElementById("overlay18").style.display = "none";
+}
+
+function on19() {
+    document.getElementById("overlay19").style.display = "flex";
+}
+function off19() {
+    document.getElementById("overlay19").style.display = "none";
+}
+
+function on20() {
+    document.getElementById("overlay20").style.display = "flex";
+}
+function off20() {
+    document.getElementById("overlay20").style.display = "none";
+}
